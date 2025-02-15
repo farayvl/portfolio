169eaf8a6255bbb65f5d0ee4318bad21d80a4113
farayvl <fara.ot.ferrari@gmail.com>
2025-02-15 08:34:30 +0500

    Deploy to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // Экспорт в статический HTML
+  images: {
+    unoptimized: true, // Отключает оптимизацию изображений (GitHub Pages не поддерживает)
+  },
+  basePath: "/portfolio", // Укажи название твоего репо (см. пункт 3)
+  assetPrefix: "/portfolio",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
